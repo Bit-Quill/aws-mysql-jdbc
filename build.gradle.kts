0562c7c90c5d970bd04332fb33a4241303c0c7f7
Colin <70862826+ColinKYuen@users.noreply.github.com>
2021-11-17 10:56:02 -0800

    chore: Publish to Maven Central on release

--- build.gradle.kts
+++ build.gradle.kts
@@ -323,7 +323,7 @@ publishing {
     repositories {
         maven {
             name = "OSSRH"
-            url = uri(layout.buildDirectory.dir("https://oss.sonatype.org/service/local/staging/deploy/maven2/"))
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
                 username = System.getenv("MAVEN_USERNAME")
                 password = System.getenv("MAVEN_PASSWORD")
