7d12fdb132488232381633cb73a3917fa00b1f31
crystall-bitquill <crystall@bitquilltech.com>
2022-02-22 09:43:46 -0800

    test: add 08001 to Hikari exceptions

--- src/test/java/testsuite/integration/hikariCP/HikariCPSQLException.java
+++ src/test/java/testsuite/integration/hikariCP/HikariCPSQLException.java
@@ -7,7 +7,8 @@ import java.sql.SQLException;
 public class HikariCPSQLException implements SQLExceptionOverride {
     public Override adjudicate(final SQLException sqlException) {
         if (sqlException.getSQLState().equalsIgnoreCase("08S02") ||
-            sqlException.getSQLState().equalsIgnoreCase("08007")) {
+            sqlException.getSQLState().equalsIgnoreCase("08007") ||
+            sqlException.getSQLState().equalsIgnoreCase("08001")) {
             return Override.DO_NOT_EVICT;
         } else {
             return Override.CONTINUE_EVICT;
